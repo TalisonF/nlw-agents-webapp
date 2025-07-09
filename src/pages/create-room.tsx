61a0c0740ecd4f53bcebb31bac0180ed471cf0e7
talisonf <talison737@gmail.com>
2025-07-08 21:18:59 -0300

    feat: add RoomList component and integrate with CreateRoom page

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,32 +1,13 @@
-import { useQuery } from '@tanstack/react-query';
-import { Link } from 'react-router-dom';
-
-type GetRoomsAPIResponse = Array<{
-  id: string;
-  name: string;
-}>;
+import { RoomList } from '@/components/room-list';
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ['get-rooms'],
-    queryFn: async () => {
-      const response = await fetch('http://localhost:3334/rooms');
-
-      const result: GetRoomsAPIResponse = await response.json();
-
-      return result;
-    },
-  });
-
   return (
-    <div>
-      {isLoading && <div>Loading...</div>}
-      <div className="flex flex-col gap-1">
-        {data?.map((room) => (
-          <Link key={room.id} to={`/room/${room.id}`}>
-            {room.name}
-          </Link>
-        ))}
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="gap 8 grid grid-cols-2 items-start gap-8">
+          <div />
+          <RoomList />
+        </div>
       </div>
     </div>
   );
