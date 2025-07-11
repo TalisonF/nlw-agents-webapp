0710721c7c452f4bb758b3bc18cc85387d64cbed
talisonf <talison737@gmail.com>
2025-07-11 00:25:09 -0300

    feat: add theme provider and mode toggle component

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@/components/theme-provider';
 import { CreateRoom } from './pages/create-room';
 import { Room } from './pages/room';
 import { UploadFilesPage } from './pages/uploadFiles';
@@ -8,14 +9,16 @@ const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<CreateRoom />} index />
-          <Route element={<Room />} path="/room/:roomId" />
-          <Route element={<UploadFilesPage />} path="/room/:roomId/dados" />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<CreateRoom />} index />
+            <Route element={<Room />} path="/room/:roomId" />
+            <Route element={<UploadFilesPage />} path="/room/:roomId/dados" />
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
